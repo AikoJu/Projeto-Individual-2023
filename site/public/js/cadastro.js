3c3fef2da07078d0f333da699ebd1e4a50da5824
AikoJu <julya.aono@sptech.school>
2023-05-21 14:19:30 -0300

    Tela de login

--- site/public/js/cadastro.js
+++
@@ -1,7 +0,0 @@
-function validacaoVazia(){
-if(nomeUsuario ==  "" || emailUsuario == "" || senhaUsuario == "" || senhaConfirmacao == ""){
-    divMensagem.style.display = `flex`
-    divMensagem.innerHTML = `Preencha todos os campos!`
-    setTimeout(()=>{divMensagem.style.display = `none`},5000)
-}
-}
